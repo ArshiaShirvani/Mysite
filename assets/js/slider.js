25458c8326b321816b7a7052a3790f47b4173939
novice-2007 <novice.developer.geek@gmail.com>
2025-03-16 01:46:50 -0700

    Slider bug fix

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,25 +1,30 @@
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3, // تعداد کارت‌ها در دسکتاپ
-    spaceBetween: 20, // فاصله بین کارت‌ها
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    loop: true, // فعال کردن حلقه برای رفتن از آخر به اول و از اول به آخر
-    autoplay: {
-      delay: 2000, // زمان تاخیر برای اسکرول هر کارت (3000 میلی‌ثانیه = 3 ثانیه)
-      disableOnInteraction: false, // اگر کاربر با اسلایدر تعامل داشته باشد، autoplay غیرفعال نمی‌شود
-    },
-    breakpoints: {
-      // برای موبایل تنظیمات را تغییر می‌دهیم
-      320: {
-        slidesPerView: 1, // یک کارت در موبایل
-      },
-      769: {
-        slidesPerView: 2, // دو کارت در تبلت
-      },
-      1025: {
-        slidesPerView: 3, // سه کارت در دسکتاپ
-      }
-    },
+$(document).ready(function () {
+  $('.slider').slick({
+      slidesToShow: 3, // تعداد کارت‌ها در دسکتاپ
+      slidesToScroll: 1, // یک کارت در هر اسکرول
+      autoplay: true,
+      autoplaySpeed: 1500,
+      prevArrow: '<button class="slick-prev"><i class="fa-solid fa-arrow-left"></i></button>',
+      nextArrow: '<button class="slick-next"><i class="fa-solid fa-arrow-right"></i></button>',
+      responsive: [
+          {
+              breakpoint: 1024,
+              settings: { slidesToShow: 2 } // دو کارت در تبلت
+          },
+          {
+              breakpoint: 600,
+              settings: { slidesToShow: 1 } // یک کارت در موبایل
+          }
+      ]
   });
+
+  // اطمینان از نمایش دکمه‌ها در هنگام hover بر روی اسلایدر
+  $('.slider').hover(
+      function () {
+          $('.slick-prev, .slick-next').css('opacity', '1');
+      },
+      function () {
+          $('.slick-prev, .slick-next').css('opacity', '0');
+      }
+  );
+});
