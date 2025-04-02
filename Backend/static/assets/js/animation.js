3fe330d9594da9ec59ac8e262f3fc28b203aef74
Arshia <arshiashirvani.f1385@gmail.com>
2025-04-02 13:18:02 +0330

    added static of the templates

---
+++ Backend/static/assets/js/animation.js
@@ -0,0 +1,26 @@
+const BoxLeft = document.querySelectorAll('.left-content');
+const BoxRight = document.querySelectorAll('.right-content');
+
+window.addEventListener('scroll',showbox);
+function showbox() {
+    const btn = window.innerHeight / 1.8;
+    
+    BoxLeft.forEach(left => {
+        const boxTop = left.getBoundingClientRect().top;
+        if (boxTop < btn) {
+            left.classList.add('show');
+        }
+        // else{
+        //     left.classList.remove('show');
+        // }
+    });
+    BoxRight.forEach(right => {
+        const boxTop = right.getBoundingClientRect().top;
+        if (boxTop < btn) {
+            right.classList.add('show');
+        }
+        // else{
+        //     right.classList.remove('show');
+        // }
+    });
+}
---
+++ Backend/static/assets/js/menu.js
@@ -0,0 +1,5 @@
+document.querySelector('#burger').addEventListener('click', function () {
+    var menu = document.querySelector('.Show-menu');
+    menu.classList.toggle('active');
+});
+
