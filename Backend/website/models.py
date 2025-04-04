ed4f2942bf06ed0209eef1070d0589e42e0f3b7c
Arshia <arshiashirvani.f1385@gmail.com>
2025-04-04 22:29:20 +0330

    write model for newsletter

--- Backend/website/models.py
+++ Backend/website/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class NewsletterModel(models.Model):
+    email = models.EmailField()
+    seen = models.BooleanField(default=False)
+    
+    created_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.email
